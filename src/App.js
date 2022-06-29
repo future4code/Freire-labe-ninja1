1f916ea4030e545a25c4f02efaf40b517c5619b0
alinegfchagas <alinegfchagas@hotmail.com>
2022-06-29 16:59:55 -0300

    filtros - início

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React from "react";
 import Header from '../src/components/header/header';
 import Homepage from "./pages/HomePage/Homepage";
+import Filtros from "./components/Filtros";
 
 
 class App extends React.Component {
+  state={
+    filtroMinimo: 0,
+    filtroMaximo: 10000000000,
+    filtroBuscaNome: ""
+  }
+  manipulaValorMinimo = (event) =>{
+
+  this.setState({filtroMinimo:event.target.value})
+  }
+
+  manipulaValorMaximo = (event) =>{
+
+  this.setState({filtroMaximo:event.target.value})
+  }
+
+  manipulaBuscaNome = (event) =>{
+
+  this.setState({filtroBuscaNome:event.target.value})
+  }
+
+
   render () {
     return (
       <div className="App">
+
         <Header/>
         
       <Homepage/>
+
+      <Filtros
+      minimo={this.state.filtroMinimo} 
+      maximo={this.state.filtroMaximo}
+      nome={this.state.filtroBuscaNome}
+      
+      onChangeMinimo={this.state.manipulaValorMinimo}
+      onChangeMaximo={this.state.manipulaValorMaximo}
+      onChangeBuscaNome={this.state.manipulaBuscaNome}
+      />
       </div>
     );
   }
---
+++ src/components/Filtros.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoxFiltro = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export default class Filtros extends React.Component {
+  
+    render() {
+    
+        return <BoxFiltro>
+
+           <label>Filtro Mínimo:
+            <input type={"number"}
+            value={this.props.minimo}
+            onChange={this.props.onChangeMinimo}/>
+           </label>
+
+           <label>Filtro Máximo:
+            <input type={"number"}
+            value={this.props.maximo}
+             onChange={this.props.onChangeMaximo}/>
+           </label>
+           
+           <label>Busca por nome:
+            <input type={"text"}
+             value={this.props.busca}
+             onChange={this.props.onChangeBuscaNome}/>
+           </label>
+
+        </BoxFiltro>;
+
+  }
+}
