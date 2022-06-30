0dab2b0a2a4b3efcb4e939955ddeb3ed0be2e510
Gustavo Willian <gustavowillianfr@outlook.com>
2022-06-29 23:27:55 -0300

    Modifiquei algumas coisas no header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ class Header extends React.Component {
                 <h2>LabeNinjas</h2>
                 <div>
                     <button >Home</button>
-                    <button >Carrinho</button>
+                    <img src="/img/carrinho.png" alt="Icone de um carrinho de compras" />
                 </div>
             </HeaderContainer>
         )
