8957319fa97aa0d7a232b17ff39d4401bf8d62d3
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-06-30 10:22:42 -0300

    url-API configurado

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,8 @@
-// (AUTHORIZATION PROJECT)
+export const BASE_URL = "https://labeninjas.herokuapp.com/auth"
+export const API_KEY = "653d8ecd-f76e-4494-99d6-662141b31319"
+
+export const headers = {
+    headers: {
+        Authorization: API_KEY
+    }
+}
